ed99133b186cfb9a70e95799f74079d6ac80df78
nidhi.sharma.rordev <nidhi.sharma.rordev@gmail.com>
2025-08-06 16:01:20 +0000

    Step 4: testcases to strip any delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,4 +13,8 @@ describe "Add the numbers in a string" do
   it "supports newlines as delimiters" do
     expect(StringCalculator.add("1\n2,3")).to eq(6)
   end
+
+  it "supports custom single-character delimiters" do
+    expect(StringCalculator.add("//;\n1;2")).to eq(3)
+  end
 end
