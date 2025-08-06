8883b5b078237fca1b0cccbb8ade9f3f1fab5bcf
nidhi.sharma.rordev <nidhi.sharma.rordev@gmail.com>
2025-08-06 16:13:35 +0000

    step 3 testcases for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,4 +17,10 @@ describe "Add the numbers in a string" do
   it "supports custom single-character delimiters" do
     expect(StringCalculator.add("//;\n1;2")).to eq(3)
   end
+
+  it "raises an error when negative numbers are present" do
+    expect {
+      StringCalculator.add("1,-2,3,-4")
+    }.to raise_error("Negatives not allowed: -2, -4")
+  end
 end
