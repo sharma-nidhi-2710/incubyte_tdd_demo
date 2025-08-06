ab6123247697e98c9c3ec57bcd2fdb4c8e7a191b
nidhi.sharma.rordev <nidhi.sharma.rordev@gmail.com>
2025-08-06 15:58:34 +0000

    Step 2: testcase to strip newline and commas

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,4 +9,8 @@ describe "Add the numbers in a string" do
     expect(StringCalculator.add("1,2")).to eq(3)
     expect(StringCalculator.add("1,2,3")).to eq(6)
   end
+
+  it "supports newlines as delimiters" do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
 end
