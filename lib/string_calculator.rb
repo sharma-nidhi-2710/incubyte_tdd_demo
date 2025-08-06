ce2915f460f4354dc1bebc75880114562af94fe5
nidhi.sharma.rordev <nidhi.sharma.rordev@gmail.com>
2025-08-06 16:14:54 +0000

    code to strip negative number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,20 @@ class StringCalculator
   def self.add(input)
     return 0 if input.empty?
 
-    numbers = input.split(/[\n,]/).map(&:to_i)
+    delimiter = /[\n,]/
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      input = parts[1]
+    end
+
+    numbers = input.split(/#{delimiter}/).map(&:to_i)
+
+    negative_numbers = numbers.select { |n| n < 0 }
+    if negative_numbers.any?
+      raise "Negatives not allowed: #{negative_numbers.join(', ')}"
+    end
+
     numbers.sum
   end
 end
